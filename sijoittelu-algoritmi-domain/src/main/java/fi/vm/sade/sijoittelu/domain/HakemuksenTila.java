0a448572f3a2873c6fbeb3aebf26b7e84d40efef
kkammone <kari.kammonen@gofore.com>
2013-07-31 17:39:03 +0300

    Harkinnanvaraisuus muutokset

--- sijoittelu-algoritmi-domain/src/main/java/fi/vm/sade/sijoittelu/domain/HakemuksenTila.java
+++ sijoittelu-algoritmi-domain/src/main/java/fi/vm/sade/sijoittelu/domain/HakemuksenTila.java
@@ -7,12 +7,12 @@ package fi.vm.sade.sijoittelu.domain;
  */
 public enum HakemuksenTila {
 
-    HYLATTY, // hakija ei voi koskaan tulla valituksi kohteeseen, hakijaa
+    HYLATTY, // hakija ei voi koskaan tulla valituksi kohteeseen
 
     VARALLA, // Hakija voi tulla kohteeseen valituksi (jossain vaiheessa)
 
-    PERUUNTUNUT, // Hakija on tullut valituksi parempaan paikkaan
-        
+    PERUUNTUNUT, // Hakija on tullut valituksi parempaan paikkaan (korkeampi hakutoive)
+
     HYVAKSYTTY, //Hakija voi ottaa paikan vastaan
 
     PERUNUT; //Hakija ei ole vastaanottanut paikkaa. Hakija ei voi tulla enää valituksi matalamman prioriteetin kohteissa
