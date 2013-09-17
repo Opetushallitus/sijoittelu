7ecdbf92d3bf95b633262dfe0d4bafac6eb8c320
jussijartamo <jussi@jartamo@gofore.com>
2013-09-17 13:34:21 +0300

    null check enum convertteriin

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/impl/converters/EnumConverter.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/impl/converters/EnumConverter.java
@@ -8,6 +8,9 @@ package fi.vm.sade.sijoittelu.tulos.service.impl.converters;
  */
 public class EnumConverter {
     public static <T extends Enum<T>> T convert(Class<T> e1, Enum<?> e2) {
+        if (e2 == null) {
+            return null;
+        }
         return Enum.valueOf(e1, e2.toString());
     }
 }
