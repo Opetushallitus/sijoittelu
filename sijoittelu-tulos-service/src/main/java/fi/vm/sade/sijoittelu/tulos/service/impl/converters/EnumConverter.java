da10be5f03b996fe6e9cee166a9d738c011d68e7
jussijartamo <jussi@jartamo@gofore.com>
2013-09-17 13:11:26 +0300

    geneerinen enum convertteri siistitty

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/impl/converters/EnumConverter.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/impl/converters/EnumConverter.java
@@ -1,8 +1,13 @@
 package fi.vm.sade.sijoittelu.tulos.service.impl.converters;
 
+/**
+ * 
+ * @author Jussi Jartamo
+ * 
+ *         Geneerinen enum convertteri
+ */
 public class EnumConverter {
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public static <T extends Enum> T convert(Class<T> e1, Enum<?> e2) {
-        return (T) Enum.valueOf(e1, e2.toString());
+    public static <T extends Enum<T>> T convert(Class<T> e1, Enum<?> e2) {
+        return Enum.valueOf(e1, e2.toString());
     }
 }
