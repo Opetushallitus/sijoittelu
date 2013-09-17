934feb824fd55296166e7654aaeab8bc9c23efe6
jussijartamo <jussi@jartamo@gofore.com>
2013-09-17 13:11:26 +0300

    geneerinen enum convertteri siistitty

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/impl/converters/EnumConverter.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/impl/converters/EnumConverter.java
@@ -3,6 +3,6 @@ package fi.vm.sade.sijoittelu.tulos.service.impl.converters;
 public class EnumConverter {
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static <T extends Enum> T convert(Class<T> e1, Enum<?> e2) {
-        return (T) Enum.valueOf(e1, e2.toString());
+        return Enum.<T> valueOf(e1, e2.toString());
     }
 }
