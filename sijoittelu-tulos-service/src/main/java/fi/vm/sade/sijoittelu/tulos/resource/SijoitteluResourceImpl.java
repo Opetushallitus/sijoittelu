d4234d4a1be5d37fe2a525ba49e1747d442f0dcc
jussijartamo <jussi@jartamo@gofore.com>
2013-09-18 10:07:04 +0300

    VT-552 mahdollisuus hakea koulutuspaikalliset tiettyyn hakukohteeseen: API-lisäys

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceImpl.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceImpl.java
@@ -1,11 +1,11 @@
 package fi.vm.sade.sijoittelu.tulos.resource;
 
-import fi.vm.sade.sijoittelu.tulos.dto.HakijaDTO;
-import fi.vm.sade.sijoittelu.tulos.dto.HakukohdeDTO;
-import fi.vm.sade.sijoittelu.tulos.dto.SijoitteluDTO;
-import fi.vm.sade.sijoittelu.tulos.dto.SijoitteluajoDTO;
-import fi.vm.sade.sijoittelu.tulos.service.RaportointiService;
-import fi.vm.sade.sijoittelu.tulos.service.SijoitteluTulosService;
+import static fi.vm.sade.sijoittelu.tulos.roles.SijoitteluRole.CRUD;
+import static fi.vm.sade.sijoittelu.tulos.roles.SijoitteluRole.READ;
+import static fi.vm.sade.sijoittelu.tulos.roles.SijoitteluRole.UPDATE;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +13,17 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 
-import static fi.vm.sade.sijoittelu.tulos.roles.SijoitteluRole.*;
+import fi.vm.sade.sijoittelu.tulos.dto.HakemuksenTila;
+import fi.vm.sade.sijoittelu.tulos.dto.HakijaDTO;
+import fi.vm.sade.sijoittelu.tulos.dto.HakukohdeDTO;
+import fi.vm.sade.sijoittelu.tulos.dto.HakutoiveDTO;
+import fi.vm.sade.sijoittelu.tulos.dto.SijoitteluDTO;
+import fi.vm.sade.sijoittelu.tulos.dto.SijoitteluajoDTO;
+import fi.vm.sade.sijoittelu.tulos.service.RaportointiService;
+import fi.vm.sade.sijoittelu.tulos.service.SijoitteluTulosService;
 
 /**
  * User: wuoti Date: 26.4.2013 Time: 12.41
@@ -65,6 +73,25 @@ public class SijoitteluResourceImpl implements SijoitteluResource {
     }
 
     @Secured({ READ, UPDATE, CRUD })
+    public List<HakijaDTO> koulutuspaikalliset(String hakuOid, final String hakukohdeOid, String sijoitteluajoId) {
+        List<HakijaDTO> hakijat = koulutuspaikalliset(hakuOid, sijoitteluajoId);
+        Collections2.filter(hakijat, new Predicate<HakijaDTO>() {
+            public boolean apply(HakijaDTO hakija) {
+                for (HakutoiveDTO toive : hakija.getHakutoiveet()) {
+                    if (HakemuksenTila.HYVAKSYTTY.equals(toive.getTila())) {
+                        if (hakukohdeOid.equals(toive.getHakukohdeOid())) {
+                            return true; // hyvaksytty oikeaan kohteeseen
+                        }
+                        return false; // hyvaksytty muuhun kohteeseen
+                    }
+                }
+                return false; // ei hakutoiveita
+            }
+        });
+        return hakijat;
+    }
+
+    @Secured({ READ, UPDATE, CRUD })
     public List<HakijaDTO> ilmankoulutuspaikkaa(String hakuOid, String sijoitteluajoId) {
         if ("latest".equals(sijoitteluajoId)) {
             return raportointiService.latestIlmankoulutuspaikkaa(hakuOid);
@@ -72,4 +99,5 @@ public class SijoitteluResourceImpl implements SijoitteluResource {
             return raportointiService.ilmankoulutuspaikkaa(Long.parseLong(sijoitteluajoId));
         }
     }
+
 }
