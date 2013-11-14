e8ac4b3a3f7f0429ae1f80d3695450d968a8056a
kkammone <kari.kammonen@gofore.com>
2013-11-14 09:55:04 +0200

    testi sijoittelun hyväksyttyjen huonompien hakutoiveiden perumiseen

--- sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluTest.java
+++ sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluTest.java
@@ -51,7 +51,7 @@ public class SijoitteluTest {
 
     @Test
     @UsingDataSet(locations = "sijoittelutestidata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
-    public void syksy2013test() {
+    public void syksy2013Test() {
         List<Hakukohde> hakukohteet = dao.getHakukohdeForSijoitteluajo(1383916417363L);
         Map<String, List<Hakemus>> hakemusMapByHakemusOid = new HashMap<String, List<Hakemus>>();
 
@@ -83,6 +83,41 @@ public class SijoitteluTest {
 
         // System.out.println PrintHelper.tulostaSijoittelu(sijoitteluAlgorithm));
     }
+
+    @Test
+    @UsingDataSet(locations = "sijoittelutestidata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void syksy2013HarkinnanvaraisestiHyvaksytytTest() {
+        List<Hakukohde> hakukohteet = dao.getHakukohdeForSijoitteluajo(1383916417363L);
+        Map<String, List<Hakemus>> hakemusMapByHakemusOid = new HashMap<String, List<Hakemus>>();
+
+        for (Hakukohde hk : hakukohteet) {
+            for (Valintatapajono vt : hk.getValintatapajonot()) {
+                for (Hakemus h : vt.getHakemukset()) {
+
+                    if (hakemusMapByHakemusOid.get(h.getHakemusOid()) != null) {
+                        List<Hakemus> list = hakemusMapByHakemusOid.get(h.getHakemusOid());
+                        list.add(h);
+                    } else {
+                        List<Hakemus> list = new ArrayList<Hakemus>();
+                        list.add(h);
+                        hakemusMapByHakemusOid.put(h.getHakemusOid(), list);
+                    }
+                }
+            }
+        }
+
+        ass(hakemusMapByHakemusOid, "1.2.246.562.11.00000011992", 1, getHakukohde(hakukohteet, "1.2.246.562.14.2013082908162538927436"), HakemuksenTila.HYVAKSYTTY);
+        ass(hakemusMapByHakemusOid, "1.2.246.562.11.00000011992", 2, getHakukohde(hakukohteet, "1.2.246.562.5.02563_04_873_0530"), HakemuksenTila.VARALLA);
+
+        SijoitteluAlgorithm sijoitteluAlgorithm = algorithmFactory.constructAlgorithm(hakukohteet, null);
+        sijoitteluAlgorithm.start();
+
+        ass(hakemusMapByHakemusOid, "1.2.246.562.11.00000011992", 1, getHakukohde(hakukohteet, "1.2.246.562.14.2013082908162538927436"), HakemuksenTila.HYVAKSYTTY);
+        ass(hakemusMapByHakemusOid, "1.2.246.562.11.00000011992", 2, getHakukohde(hakukohteet, "1.2.246.562.5.02563_04_873_0530"), HakemuksenTila.PERUUNTUNUT);
+
+        // System.out.println PrintHelper.tulostaSijoittelu(sijoitteluAlgorithm));
+    }
+
 
     private Hakukohde getHakukohde(List<Hakukohde> hakukohteet, String hakukohdeOid) {
         for (Hakukohde hakukohde : hakukohteet) {
