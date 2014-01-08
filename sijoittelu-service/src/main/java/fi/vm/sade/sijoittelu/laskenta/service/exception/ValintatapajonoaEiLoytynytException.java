94af326da3020e34b0d929cab3bbe5490e099daa
Juho Kaistinen <jukais@gofore.com>
2014-01-08 15:22:33 +0200

    Sonar fixauksia

---
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/exception/ValintatapajonoaEiLoytynytException.java
@@ -0,0 +1,10 @@
+package fi.vm.sade.sijoittelu.laskenta.service.exception;
+
+public class ValintatapajonoaEiLoytynytException extends RuntimeException {
+
+    private static final long serialVersionUID = -3179202462019627447L;
+
+    public ValintatapajonoaEiLoytynytException(String s) {
+        super(s);
+    }
+}
