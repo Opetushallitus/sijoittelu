fbcdafeac2ef3a4663e44339a2627eb01babf4f0
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-08-27 10:33:33 +0300

    Expect result of resource to equal API response

--- sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
+++ sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.sijoittelu.tulos.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
@@ -15,13 +17,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 import static org.junit.Assert.assertEquals;
 
-/**
- * @author blomqvie
- */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:test-context.xml"})
 public class SijoitteluResourceTest {
+
     @Rule
     public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("sijoittelu");
 
@@ -31,14 +30,20 @@ public class SijoitteluResourceTest {
     @Autowired
     SijoitteluResource sijoitteluResource;
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     @UsingDataSet(locations = "sijoittelu-tulos-testdata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
-    public void asdf() {
+    public void resultForApplication() throws JsonProcessingException {
+
+        String expected = "{\"hakemusOid\":\"1.2.246.562.11.00000878229\",\"etunimi\":\"Teppo\",\"sukunimi\":\"Testaaja\",\"hakutoiveet\":[{\"hakutoive\":1,\"hakukohdeOid\":\"1.2.246.562.20.83060182827\",\"tarjoajaOid\":\"1.2.246.562.10.83122281013\",\"pistetiedot\":[],\"hakutoiveenValintatapajonot\":[{\"valintatapajonoPrioriteetti\":1,\"valintatapajonoOid\":\"14090336922663576781797489829886\",\"valintatapajonoNimi\":\"Varsinainen jono\",\"jonosija\":1,\"paasyJaSoveltuvuusKokeenTulos\":null,\"varasijanNumero\":null,\"tila\":\"HYVAKSYTTY\",\"tilanKuvaukset\":{},\"vastaanottotieto\":\"ILMOITETTU\",\"ilmoittautumisTila\":null,\"hyvaksyttyHarkinnanvaraisesti\":false,\"tasasijaJonosija\":1,\"pisteet\":4,\"alinHyvaksyttyPistemaara\":4,\"hakeneet\":1,\"hyvaksytty\":1,\"varalla\":0}]}]}";
+
         String hakuOid = "1.2.246.562.29.92478804245";
         String sijoitteluAjoId = "latest";
         String hakemusOid = "1.2.246.562.11.00000878229";
         HakijaDTO hakemus = sijoitteluResource.hakemus(hakuOid, sijoitteluAjoId, hakemusOid);
-
         assertEquals("Teppo", hakemus.getEtunimi());
+        assertEquals(expected, objectMapper.writeValueAsString(hakemus));
+
     }
 }
