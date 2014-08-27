71b0ed713c104776017705e22209fde1c7cd5867
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-08-27 14:49:14 +0300

    Add test for yhteenveto resources

--- sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
+++ sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakemusYhteenvetoDTO;
 import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO;
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,18 +33,24 @@ public class SijoitteluResourceTest {
 
     ObjectMapper objectMapper = new ObjectMapper();
 
+    String hakuOid = "1.2.246.562.29.92478804245";
+    String sijoitteluAjoId = "latest";
+    String hakemusOid = "1.2.246.562.11.00000878229";
+
     @Test
     @UsingDataSet(locations = "sijoittelu-tulos-testdata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void resultForApplication() throws JsonProcessingException {
-
         String expected = "{\"hakemusOid\":\"1.2.246.562.11.00000878229\",\"etunimi\":\"Teppo\",\"sukunimi\":\"Testaaja\",\"hakutoiveet\":[{\"hakutoive\":1,\"hakukohdeOid\":\"1.2.246.562.20.83060182827\",\"tarjoajaOid\":\"1.2.246.562.10.83122281013\",\"pistetiedot\":[],\"hakutoiveenValintatapajonot\":[{\"valintatapajonoPrioriteetti\":1,\"valintatapajonoOid\":\"14090336922663576781797489829886\",\"valintatapajonoNimi\":\"Varsinainen jono\",\"jonosija\":1,\"paasyJaSoveltuvuusKokeenTulos\":null,\"varasijanNumero\":null,\"tila\":\"HYVAKSYTTY\",\"tilanKuvaukset\":{},\"vastaanottotieto\":\"ILMOITETTU\",\"ilmoittautumisTila\":null,\"hyvaksyttyHarkinnanvaraisesti\":false,\"tasasijaJonosija\":1,\"pisteet\":4,\"alinHyvaksyttyPistemaara\":4,\"hakeneet\":1,\"hyvaksytty\":1,\"varalla\":0}]}]}";
-
-        String hakuOid = "1.2.246.562.29.92478804245";
-        String sijoitteluAjoId = "latest";
-        String hakemusOid = "1.2.246.562.11.00000878229";
         HakijaDTO hakemus = sijoitteluResource.hakemus(hakuOid, sijoitteluAjoId, hakemusOid);
         assertEquals("Teppo", hakemus.getEtunimi());
         assertEquals(expected, objectMapper.writeValueAsString(hakemus));
+    }
 
+    @Test
+    @UsingDataSet(locations = "sijoittelu-tulos-testdata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void yhteenveto() throws JsonProcessingException {
+        String expectedResponse = "{\"hakemusOid\":\"1.2.246.562.11.00000878229\",\"hakutoiveet\":[{\"hakukohdeOid\":\"1.2.246.562.20.83060182827\",\"tarjoajaOid\":\"1.2.246.562.10.83122281013\",\"tila\":\"HYVAKSYTTY\",\"vastaanottotieto\":\"ILMOITETTU\",\"ilmoittautumisTila\":null,\"jonosija\":1,\"varasijanNumero\":null}]}";
+        HakemusYhteenvetoDTO yhteenveto = sijoitteluResource.hakemusYhteenveto(hakuOid, sijoitteluAjoId, hakemusOid);
+        assertEquals(expectedResponse, objectMapper.writeValueAsString(yhteenveto));
     }
 }
