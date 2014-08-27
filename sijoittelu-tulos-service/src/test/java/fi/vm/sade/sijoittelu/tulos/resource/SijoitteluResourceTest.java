9c8af95a6f2957336a104315643a83832494ef9d
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-08-27 10:24:00 +0300

    Initial test functionality

--- sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
+++ sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
@@ -3,6 +3,7 @@ package fi.vm.sade.sijoittelu.tulos.resource;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaDTO;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author blomqvie
@@ -20,15 +22,23 @@ import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:test-context.xml"})
 public class SijoitteluResourceTest {
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Rule
     public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("sijoittelu");
 
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    SijoitteluResource sijoitteluResource;
+
     @Test
     @UsingDataSet(locations = "sijoittelu-tulos-testdata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
-    public void asdf() {x   
+    public void asdf() {
+        String hakuOid = "1.2.246.562.29.92478804245";
+        String sijoitteluAjoId = "latest";
+        String hakemusOid = "1.2.246.562.11.00000878229";
+        HakijaDTO hakemus = sijoitteluResource.hakemus(hakuOid, sijoitteluAjoId, hakemusOid);
 
+        assertEquals("Teppo", hakemus.getEtunimi());
     }
 }
