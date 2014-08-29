db399247fd2ae6bd2fbf1fb19d7acece9484123e
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-08-29 16:33:15 +0300

    Split mock data to different files

--- sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
+++ sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/resource/SijoitteluResourceTest.java
@@ -38,7 +38,7 @@ public class SijoitteluResourceTest {
     String hakemusOid = "1.2.246.562.11.00000441369";
 
     @Test
-    @UsingDataSet(locations = "sijoittelu-tulos-mockdata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    @UsingDataSet(locations = {"sijoittelu-base-mockdata.json", "sijoittelu-tulos-mockdata.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void resultForApplication() throws JsonProcessingException {
         String expected = "{\"hakemusOid\":\"1.2.246.562.11.00000441369\",\"etunimi\":\"Teppo\",\"sukunimi\":\"Testaaja\",\"hakutoiveet\":[{\"hakutoive\":1,\"hakukohdeOid\":\"1.2.246.562.5.72607738902\",\"tarjoajaOid\":\"1.2.246.562.10.591352080610\",\"pistetiedot\":[],\"hakutoiveenValintatapajonot\":[{\"valintatapajonoPrioriteetti\":1,\"valintatapajonoOid\":\"14090336922663576781797489829886\",\"valintatapajonoNimi\":\"Varsinainen jono\",\"jonosija\":1,\"paasyJaSoveltuvuusKokeenTulos\":null,\"varasijanNumero\":null,\"tila\":\"HYVAKSYTTY\",\"tilanKuvaukset\":{},\"vastaanottotieto\":\"ILMOITETTU\",\"ilmoittautumisTila\":null,\"hyvaksyttyHarkinnanvaraisesti\":false,\"tasasijaJonosija\":1,\"pisteet\":4,\"alinHyvaksyttyPistemaara\":4,\"hakeneet\":1,\"hyvaksytty\":1,\"varalla\":0,\"varasijat\":0,\"varasijaTayttoPaivat\":0,\"varasijojaKaytetaanAlkaen\":null,\"varasijojaTaytetaanAsti\":null,\"tayttojono\":null}],\"kaikkiJonotSijoiteltu\":true}]}";
         HakijaDTO hakemus = sijoitteluResource.hakemus(hakuOid, sijoitteluAjoId, hakemusOid);
@@ -47,7 +47,7 @@ public class SijoitteluResourceTest {
     }
 
     @Test
-    @UsingDataSet(locations = "sijoittelu-tulos-mockdata.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    @UsingDataSet(locations = {"sijoittelu-base-mockdata.json", "sijoittelu-tulos-mockdata.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void yhteenveto() throws JsonProcessingException {
         String expectedResponse = "{\"hakemusOid\":\"1.2.246.562.11.00000441369\",\"hakutoiveet\":[{\"hakukohdeOid\":\"1.2.246.562.5.72607738902\",\"tarjoajaOid\":\"1.2.246.562.10.591352080610\",\"tila\":\"HYVAKSYTTY\",\"vastaanottotieto\":\"ILMOITETTU\",\"ilmoittautumisTila\":null,\"jonosija\":1,\"varasijanNumero\":null}]}";
         HakemusYhteenvetoDTO yhteenveto = sijoitteluResource.hakemusYhteenveto(hakuOid, sijoitteluAjoId, hakemusOid);
