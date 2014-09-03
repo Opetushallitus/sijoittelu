814e4a68a8dd5f9b3ebdc10fbe3895c630437562
Juha Paananen <juha.paananen@gmail.com>
2014-09-03 15:13:59 +0300

    fetch data for requested Hakemus only, to improve performance

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/dao/impl/HakukohdeDaoImpl.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/dao/impl/HakukohdeDaoImpl.java
@@ -1,14 +1,27 @@
 package fi.vm.sade.sijoittelu.tulos.dao.impl;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.annotation.PostConstruct;
 
 import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.MapreduceType;
+import org.mongodb.morphia.mapping.Mapper;
+import org.mongodb.morphia.mapping.cache.DefaultEntityCache;
 import org.mongodb.morphia.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Repository;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MapReduceCommand;
+import com.mongodb.MapReduceOutput;
 
 import fi.vm.sade.sijoittelu.domain.Hakukohde;
 import fi.vm.sade.sijoittelu.tulos.dao.HakukohdeDao;
@@ -45,12 +58,24 @@ public class HakukohdeDaoImpl implements HakukohdeDao {
     }
 
     @Override
-    public List<Hakukohde> haeHakukohteetJoihinHakemusOsallistuu(
-        Long sijoitteluajoId, String hakemusOid) {
-        Query<Hakukohde> query = morphiaDS.createQuery(Hakukohde.class);
-        query.field("sijoitteluajoId").equal(sijoitteluajoId);
-        query.field("valintatapajonot.hakemukset.hakemusOid").equal(hakemusOid);
-        List<Hakukohde> list = query.asList();
-        return list;
+    public List<Hakukohde> haeHakukohteetJoihinHakemusOsallistuu(Long sijoitteluajoId, String hakemusOid) {
+        final BasicDBObject query = new BasicDBObject("sijoitteluajoId", sijoitteluajoId).append("valintatapajonot.hakemukset.hakemusOid", hakemusOid);
+
+        String map = "function() { emit(this.oid, {className:this.className, sijoitteluajoId: this.sijoitteluajoId, oid:this.oid, tarjoajaId: this.tarjoajaId, kaikkiJonotSijoiteltu: this.kaikkiJonotSijoiteltu, valintatapajonot: this.valintatapajonot.map(function(jono) { return {tasasijasaanto: jono.tasasijasaanto, oid:jono.oid, prioriteetti: jono.prioriteetti, aloituspaikat:jono.aloituspaikat, eiVarasijatayttoa:jono.eiVarasijatayttoa, kaikkiEhdonTayttavatHyvaksytaan: jono.kaikkiEhdonTayttavatHyvaksytaan, poissaOlevaTaytto:jono.poissaOlevaTaytto, varasijat:jono.varasijat, varasijaTayttoPaivat: jono.varasijaTayttoPaivat, hakemukset:jono.hakemukset.filter(function(hakemus) { return hakemus.hakemusOid == '"+hakemusOid+"' })}})})}";
+        String reduce = "function(key, values) { return values[0] }";
+
+        DBCollection collection = morphiaDS.getCollection(Hakukohde.class);
+        MapReduceCommand cmd = new MapReduceCommand(
+            collection,
+            map,
+            reduce,
+            null,
+            MapReduceCommand.OutputType.INLINE,
+            query);
+        MapReduceOutput out = collection.mapReduce(cmd);
+
+        return StreamSupport.stream(out.results().spliterator(), false).map(dbObject -> {
+            return new Mapper().fromDBObject(Hakukohde.class, (DBObject) dbObject.get("value"), new DefaultEntityCache());
+        }).collect(Collectors.toList());
     }
 }
