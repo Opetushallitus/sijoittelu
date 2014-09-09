cd2fa4f4bc76b5b52c9061a0d8c941c88020883b
Kalle Manninen <kalle.manninen@gofore.com>
2014-09-09 16:55:13 +0300

    VT-773 Selite sijoittelussa peruuntunut-tilaisille hakijoille

--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorPeruutaAlemmatPeruneetJaHyvaksytyt.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorPeruutaAlemmatPeruneetJaHyvaksytyt.java
@@ -28,8 +28,8 @@ public class PreSijoitteluProcessorPeruutaAlemmatPeruneetJaHyvaksytyt implements
                     if(parasHyvaksyttyHakutoive != null) {
                         if(hakemusWrapper.isTilaVoidaanVaihtaa() && hakemusWrapper.getHakemus().getTila() == HakemuksenTila.VARALLA && hakemusWrapper.getHakemus().getPrioriteetti() >= parasHyvaksyttyHakutoive) {
                             hakemusWrapper.getHakemus().getTilanKuvaukset().put("FI","Peruuntunut, hyväksytty ylemmälle hakutoiveelle");
-                            hakemusWrapper.getHakemus().getTilanKuvaukset().put("SV","Peruuntunut, hyväksytty ylemmälle hakutoiveelle");
-                            hakemusWrapper.getHakemus().getTilanKuvaukset().put("EN","Peruuntunut, hyväksytty ylemmälle hakutoiveelle");
+                            hakemusWrapper.getHakemus().getTilanKuvaukset().put("SV","Annullerad, godkänt till ansökningsmål med högre prioritet");
+                            hakemusWrapper.getHakemus().getTilanKuvaukset().put("EN","Cancelled, accepted for a study place with higher priority");
                             hakemusWrapper.getHakemus().setTila(HakemuksenTila.PERUUNTUNUT);
                         }
                     }
