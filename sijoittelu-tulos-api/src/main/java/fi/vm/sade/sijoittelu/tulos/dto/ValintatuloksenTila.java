55c27618041c3c20d228c2292e4fb5c950d1adb6
kjsaila <kalle.saila@gofore.com>
2014-09-10 13:15:37 +0300

    defaultit tiloille dto:hon

--- sijoittelu-tulos-api/src/main/java/fi/vm/sade/sijoittelu/tulos/dto/ValintatuloksenTila.java
+++ sijoittelu-tulos-api/src/main/java/fi/vm/sade/sijoittelu/tulos/dto/ValintatuloksenTila.java
@@ -15,5 +15,6 @@ public enum ValintatuloksenTila {
     EI_VASTAANOTETTU_MAARA_AIKANA, // Hakija ei ole ilmoittanut paikkaa vastaanotetuksi maaraaikana ja on nain ollen hylatty
     PERUNUT,                       // Hakija ei ota paikkaa vastaan
     PERUUTETTU,                    // Hakijan tila on peruutettu
-    EHDOLLISESTI_VASTAANOTTANUT    // Ehdollisesti vastaanottanut
+    EHDOLLISESTI_VASTAANOTTANUT,    // Ehdollisesti vastaanottanut
+    KESKEN
 }
