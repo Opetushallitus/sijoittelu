849a4d3ed68995255047323c3300e4616f1a36fd
Juha Paananen <juha.paananen@gmail.com>
2014-09-18 11:31:38 +0300

    add parameter to FixtureImporter.importFixtures

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/testfixtures/FixtureImporter.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/testfixtures/FixtureImporter.java
@@ -4,10 +4,10 @@ import com.mongodb.DB;
 import com.mongodb.DBObject;
 
 public class FixtureImporter {
-    public static void importFixtures(final DB db) {
+    public static void importFixtures(final DB db, final String fixtureName) {
         DBObject base = MongoMockData.readJson("fi/vm/sade/sijoittelu/tulos/resource/sijoittelu-basedata.json");
         MongoMockData.insertData(db, base);
-        DBObject tulokset = MongoMockData.readJson("fi/vm/sade/sijoittelu/tulos/resource/hyvaksytty-ilmoitettu.json");
+        DBObject tulokset = MongoMockData.readJson("fi/vm/sade/sijoittelu/tulos/resource/"+ fixtureName);
         MongoMockData.insertData(db, tulokset);
     }
 }
