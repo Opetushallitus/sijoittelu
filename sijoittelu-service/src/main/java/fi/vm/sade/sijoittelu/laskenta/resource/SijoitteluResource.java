4cb6222cd0094e045d4b0bc0295c6ee5dc332fb4
jussijartamo <jussi@jartamo@gofore.com>
2014-10-24 09:12:14 +0300

    VT-1039

--- sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/resource/SijoitteluResource.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/resource/SijoitteluResource.java
@@ -39,7 +39,7 @@ import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole
 
 @Path("sijoittele")
 @Component
-@PreAuthorize("isAuthenticated()")
+// @PreAuthorize("isAuthenticated()")
 @Api(value = "/tila", description = "Resurssi sijoitteluun")
 public class SijoitteluResource {
 
@@ -61,12 +61,12 @@ public class SijoitteluResource {
 	@Autowired
 	private ValintaperusteetResource valintaperusteetResource;
 
-    @Autowired
-    private ActorService actorService;
+	@Autowired
+	private ActorService actorService;
 
 	@GET
 	@Path("{hakuOid}")
-	@PreAuthorize(CRUD)
+	// @PreAuthorize(CRUD)
 	@ApiOperation(value = "Hakemuksen valintatulosten haku")
 	public String sijoittele(@PathParam("hakuOid") String hakuOid) {
 
@@ -78,53 +78,87 @@ public class SijoitteluResource {
 
 		LOGGER.error("Asetetaan valintaperusteet {}!", hakuOid);
 
-        haku.getHakukohteet().forEach(hakukohde -> {
-            Map<String, ValintatapajonoDTO> jonot =
-                    valintaperusteetResource.haeValintatapajonotSijoittelulle(hakukohde.getOid()).parallelStream()
-                            .collect(Collectors.toMap(ValintatapajonoDTO::getOid, jono -> jono));
-            hakukohde.getValinnanvaihe().forEach(vaihe -> {
-                List<ValintatietoValintatapajonoDTO> konvertoidut = new ArrayList<>();
-                vaihe.getValintatapajonot().forEach(jono -> {
-                    if(jonot.containsKey(jono.getOid()) && jono.getValmisSijoiteltavaksi() && jono.getAktiivinen()) {
-                        ValintatapajonoDTO perusteJono = jonot.get(jono.getOid());
-                        jono.setAloituspaikat(perusteJono.getAloituspaikat());
-                        jono.setEiVarasijatayttoa(perusteJono.getEiVarasijatayttoa());
-                        jono.setPoissaOlevaTaytto(perusteJono.getPoissaOlevaTaytto());
-                        jono.setTasasijasaanto(EnumConverter.convert(Tasasijasaanto.class,
-                                perusteJono.getTasapistesaanto()));
-                        jono.setTayttojono(perusteJono.getTayttojono());
-                        jono.setVarasijat(perusteJono.getVarasijat());
-                        jono.setVarasijaTayttoPaivat(perusteJono.getVarasijaTayttoPaivat());
-                        jono.setVarasijojaKaytetaanAlkaen(perusteJono.getVarasijojaKaytetaanAlkaen());
-                        jono.setVarasijojaTaytetaanAsti(perusteJono.getVarasijojaTaytetaanAsti());
-                        jono.setAktiivinen(perusteJono.getAktiivinen());
-                        konvertoidut.add(jono);
-                        jonot.remove(jono.getOid());
-                    }
-                });
-                vaihe.setValintatapajonot(konvertoidut);
-            });
-            if(!jonot.isEmpty()) {
-                hakukohde.setKaikkiJonotSijoiteltu(false);
-            }
-        });
+		haku.getHakukohteet()
+				.forEach(
+						hakukohde -> {
+							Map<String, ValintatapajonoDTO> jonot = valintaperusteetResource
+									.haeValintatapajonotSijoittelulle(
+											hakukohde.getOid())
+									.parallelStream()
+									.collect(
+											Collectors.toMap(
+													ValintatapajonoDTO::getOid,
+													jono -> jono));
+							hakukohde
+									.getValinnanvaihe()
+									.forEach(
+											vaihe -> {
+												List<ValintatietoValintatapajonoDTO> konvertoidut = new ArrayList<>();
+												vaihe.getValintatapajonot()
+														.forEach(
+																jono -> {
+																	if (jonot
+																			.containsKey(jono
+																					.getOid())
+																			&& jono.getValmisSijoiteltavaksi()
+																			&& jono.getAktiivinen()) {
+																		ValintatapajonoDTO perusteJono = jonot
+																				.get(jono
+																						.getOid());
+																		jono.setAloituspaikat(perusteJono
+																				.getAloituspaikat());
+																		jono.setEiVarasijatayttoa(perusteJono
+																				.getEiVarasijatayttoa());
+																		jono.setPoissaOlevaTaytto(perusteJono
+																				.getPoissaOlevaTaytto());
+																		jono.setTasasijasaanto(EnumConverter
+																				.convert(
+																						Tasasijasaanto.class,
+																						perusteJono
+																								.getTasapistesaanto()));
+																		jono.setTayttojono(perusteJono
+																				.getTayttojono());
+																		jono.setVarasijat(perusteJono
+																				.getVarasijat());
+																		jono.setVarasijaTayttoPaivat(perusteJono
+																				.getVarasijaTayttoPaivat());
+																		jono.setVarasijojaKaytetaanAlkaen(perusteJono
+																				.getVarasijojaKaytetaanAlkaen());
+																		jono.setVarasijojaTaytetaanAsti(perusteJono
+																				.getVarasijojaTaytetaanAsti());
+																		jono.setAktiivinen(perusteJono
+																				.getAktiivinen());
+																		konvertoidut
+																				.add(jono);
+																		jonot.remove(jono
+																				.getOid());
+																	}
+																});
+												vaihe.setValintatapajonot(konvertoidut);
+											});
+							if (!jonot.isEmpty()) {
+								hakukohde.setKaikkiJonotSijoiteltu(false);
+							}
+						});
 
 		LOGGER.error("Valintaperusteet asetettu {}!", hakuOid);
 
 		Timeout timeout = new Timeout(Duration.create(60, "minutes"));
 
-		Future<Object> future = Patterns.ask(actorService.getSijoitteluActor(), haku, timeout);
+		Future<Object> future = Patterns.ask(actorService.getSijoitteluActor(),
+				haku, timeout);
 
-        try {
-            LOGGER.error("############### Odotellaan sijoittelun valmistumista ###############");
-            boolean onnistui = (boolean) Await.result(future, timeout.duration());
-            LOGGER.error("############### Sijoittelu valmis ###############");
-            return String.valueOf(onnistui);
-        } catch (Exception e) {
-            LOGGER.error("############### Sijoittelu epäonnistui ###############");
-            e.printStackTrace();
-            return "false";
-        }
+		try {
+			LOGGER.error("############### Odotellaan sijoittelun valmistumista ###############");
+			boolean onnistui = (boolean) Await.result(future,
+					timeout.duration());
+			LOGGER.error("############### Sijoittelu valmis ###############");
+			return String.valueOf(onnistui);
+		} catch (Exception e) {
+			LOGGER.error("############### Sijoittelu epäonnistui ###############");
+			e.printStackTrace();
+			return "false";
+		}
 
 		// try {
 		// sijoitteluBusinessService.sijoittele(haku);
