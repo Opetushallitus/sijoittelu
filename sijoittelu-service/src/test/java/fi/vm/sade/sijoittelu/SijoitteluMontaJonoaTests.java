b530d4c29c5ce1dd3a0409ce9a3e5824641deb2d
kjsaila <kalle.saila@gofore.com>
2014-11-24 21:23:44 +0200

    Testikorjaus

--- sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluMontaJonoaTests.java
+++ sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluMontaJonoaTests.java
@@ -314,7 +314,7 @@ public class SijoitteluMontaJonoaTests {
         hakukohteet.get(0).getValintatapajonot().get(0).getHakemukset().forEach(hak -> {
             if(hak.getHakemusOid().equals("1.2.246.562.11.00001067411")) {
                 Assert.assertTrue(hak.getTila().equals(HakemuksenTila.HYVAKSYTTY));
-                Assert.assertEquals(hak.getTilanKuvaukset().get("FI"), TilanKuvaukset.hyvaksyttyTayttojonoSaannolla("Pisteet").get("FI"));
+                Assert.assertEquals(hak.getTilanKuvaukset().get("FI"), TilanKuvaukset.hyvaksyttyTayttojonoSaannolla("Koe").get("FI"));
             }
         });
 
