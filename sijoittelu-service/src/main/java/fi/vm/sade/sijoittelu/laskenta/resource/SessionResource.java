2cf8e9d235447eb2ebfe645ce497fa6159f64776
August Kilponen <august.kilponen@gmail.com>
2015-04-27 16:45:41 +0300

    BUG-304: Session vanhenemisilmoitus

---
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/resource/SessionResource.java
@@ -0,0 +1,32 @@
+package fi.vm.sade.sijoittelu.laskenta.resource;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("session")
+@Api(value = "/session", description = "Sessionhallinta")
+public class SessionResource {
+
+    @GET
+    @Path("/maxinactiveinterval")
+    @PreAuthorize("isAuthenticated()")
+    @Produces(MediaType.TEXT_PLAIN)
+    @ApiOperation(
+            value = "Palauttaa session erääntymisen aikarajan sekunteina",
+            notes = "Tarvitsee HTTP kutsun, jossa on session id",
+            response = String.class)
+    public String maxInactiveInterval(@Context HttpServletRequest req) {
+        return Integer.toString(req.getSession().getMaxInactiveInterval());
+    }
+}
