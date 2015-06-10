a30dc99180e9bf79a6e4cc5a7d7185e191d1a30a
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-10 23:10:48 +0300

    reformat and remove some javadoc

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/RaportointiService.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/RaportointiService.java
@@ -7,16 +7,7 @@ import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakijaPaginationObject;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kkammone
- * Date: 16.9.2013
- * Time: 14:50
- * To change this template use File | Settings | File Templates.
- */
 public interface RaportointiService {
-
-
     Optional<SijoitteluAjo> getSijoitteluAjo(Long SijoitteluajoId);
 
     Optional<SijoitteluAjo> latestSijoitteluAjoForHaku(String hakuOid);
--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/SijoitteluTulosService.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/service/SijoitteluTulosService.java
@@ -5,12 +5,7 @@ import fi.vm.sade.sijoittelu.tulos.dto.HakukohdeDTO;
 import fi.vm.sade.sijoittelu.tulos.dto.SijoitteluDTO;
 import fi.vm.sade.sijoittelu.tulos.dto.SijoitteluajoDTO;
 
-/**
- * Created with IntelliJ IDEA. User: kkammone Date: 5.9.2013 Time: 14:22 To
- * change this template use File | Settings | File Templates.
- */
 public interface SijoitteluTulosService {
-
     HakukohdeDTO getHakukohdeBySijoitteluajo(SijoitteluAjo sijoitteluAjo, String hakukohdeOid);
 
     SijoitteluajoDTO getSijoitteluajo(SijoitteluAjo sijoitteluAjo);
