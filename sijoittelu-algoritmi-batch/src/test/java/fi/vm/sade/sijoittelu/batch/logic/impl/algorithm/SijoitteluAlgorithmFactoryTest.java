18e7601887bfe50322a8b6b2f5a1a6fd4a5e9265
Kari Husa <kari.husa@reaktor.fi>
2015-07-22 15:11:38 +0300

    Fix test

--- sijoittelu-algoritmi-batch/src/test/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/SijoitteluAlgorithmFactoryTest.java
+++ sijoittelu-algoritmi-batch/src/test/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/SijoitteluAlgorithmFactoryTest.java
@@ -96,19 +96,25 @@ public class SijoitteluAlgorithmFactoryTest {
     public void testconstructAlgorithm_PERUUNTUNUT() {
         Valintatulos valintatulos = valintatulosWithTila(ValintatuloksenTila.KESKEN);
         valintatulos.setHyvaksyPeruuntunut(true);
-        SijoitteluAlgorithm sijoitteluAlgorithm = sijoitteluAlgorithm(valintatulos, HakemuksenTila.PERUUNTUNUT);
+        List<Hakemus> hakemukset = generateHakemukset(HakemuksenTila.PERUUNTUNUT, HakemuksenTila.PERUUNTUNUT);
+        SijoitteluAlgorithm sijoitteluAlgorithm = sijoitteluAlgorithm(valintatulos, hakemukset);
 
         HakemusWrapper hakemusWrapper = sijoitteluAlgorithm.sijoitteluAjo.getHakukohteet().get(0).getValintatapajonot().get(0).getHakemukset().get(0);
-        assertEquals(hakemusWrapper.getHakemus().getTila(), HakemuksenTila.HYVAKSYTTY);
+        assertEquals(HakemuksenTila.HYVAKSYTTY, hakemusWrapper.getHakemus().getTila());
         assertFalse(hakemusWrapper.isTilaVoidaanVaihtaa());
     }
 
     private SijoitteluAlgorithm sijoitteluAlgorithm(Valintatulos valintatulos) {
-        return sijoitteluAlgorithm(valintatulos, HakemuksenTila.HYVAKSYTTY);
+        final Hakemus hakemus = new Hakemus();
+        hakemus.setHakemusOid("123");
+        return sijoitteluAlgorithm(valintatulos, Collections.singletonList(hakemus));
     }
 
-    private SijoitteluAlgorithm sijoitteluAlgorithm(Valintatulos valintatulos, HakemuksenTila tila) {
-        return sijoitteluAlgorithmFactory.constructAlgorithm(generateHakukohteet(tila), Collections.singletonList(valintatulos));
+    private SijoitteluAlgorithm sijoitteluAlgorithm(Valintatulos valintatulos, List<Hakemus> hakemukset) {
+        List<Valintatapajono> valintatapajonot = generateValintatapajono(hakemukset);
+        final List<Hakukohde> hakukohteet = generateHakukohteet(valintatapajonot);
+        final List<Valintatulos> valintatulokset = Collections.singletonList(valintatulos);
+        return sijoitteluAlgorithmFactory.constructAlgorithm(hakukohteet, valintatulokset);
     }
 
     private Valintatulos valintatulosWithTila(ValintatuloksenTila tila) {
@@ -120,24 +126,24 @@ public class SijoitteluAlgorithmFactoryTest {
         return valintatulos;
     }
 
-    private List<Hakukohde> generateHakukohteet(HakemuksenTila tila) {
+    private List<Hakukohde> generateHakukohteet(List<Valintatapajono> valintatapajonot) {
         Hakukohde hakukohde = new Hakukohde();
-        hakukohde.setValintatapajonot(generateValintatapajono(tila));
+        hakukohde.setValintatapajonot(valintatapajonot);
         hakukohde.setOid("123");
         return Collections.singletonList(hakukohde);
     }
 
-    private List<Valintatapajono> generateValintatapajono(HakemuksenTila tila) {
+    private List<Valintatapajono> generateValintatapajono(List<Hakemus> hakemukset) {
         Valintatapajono valintatapajono = new Valintatapajono();
         valintatapajono.setOid("123");
-        List<Hakemus> hakemukset = generateHakemukset(tila);
         valintatapajono.setHakemukset(hakemukset);
         return Collections.singletonList(valintatapajono);
     }
 
-    private List<Hakemus> generateHakemukset(HakemuksenTila tila) {
+    private List<Hakemus> generateHakemukset(HakemuksenTila edellinenTila, HakemuksenTila tila) {
         Hakemus hakemus = new Hakemus();
         hakemus.setHakemusOid("123");
+        hakemus.setEdellinenTila(edellinenTila);
         hakemus.setTila(tila);
         return Collections.singletonList(hakemus);
     }
