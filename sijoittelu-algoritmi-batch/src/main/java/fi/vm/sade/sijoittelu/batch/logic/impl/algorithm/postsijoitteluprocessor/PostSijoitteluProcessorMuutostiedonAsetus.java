e2705c9428fb2304940ce09c4e3af5e33270f8e3
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-07-22 12:55:40 +0300

    debug loggaus post ja pre prossesoriin

--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/postsijoitteluprocessor/PostSijoitteluProcessorMuutostiedonAsetus.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/postsijoitteluprocessor/PostSijoitteluProcessorMuutostiedonAsetus.java
@@ -1,13 +1,17 @@
 package fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.postsijoitteluprocessor;
 
+import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
 import com.google.common.hash.Hashing;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Jussi Jartamo
  */
 public class PostSijoitteluProcessorMuutostiedonAsetus implements PostSijoitteluProcessor{
+    private static final Logger LOG = LoggerFactory.getLogger(PostSijoitteluProcessorMuutostiedonAsetus.class);
     @Override
     public String name() {
         return getClass().getSimpleName();
@@ -19,6 +23,10 @@ public class PostSijoitteluProcessorMuutostiedonAsetus implements PostSijoittelu
         sijoitteluajoWrapper.getHakukohteet().stream()
                 .flatMap(h -> h.getValintatapajonot().stream())
                 .flatMap(v -> v.getHakemukset().stream())
-                .forEach(h ->h.getHakemus().setOnkoMuuttunutViimeSijoittelussa(h.luoHash().equals(h.getLahtotilanteenHash())));
+                .forEach(h -> {
+                    HashCode hc = null;
+                    h.getHakemus().setOnkoMuuttunutViimeSijoittelussa((hc = h.luoHash()).equals(h.getLahtotilanteenHash()));
+                    //LOG.error("## {} {}", h.getHakemus().getHakemusOid(), hc);
+                });
     }
 }
--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorLahtotilanteenHash.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorLahtotilanteenHash.java
@@ -6,6 +6,8 @@ import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.HakemusWrapper;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.HakukohdeWrapper;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.ValintatapajonoWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Jussi Jartamo
  */
 public class PreSijoitteluProcessorLahtotilanteenHash implements PreSijoitteluProcessor {
+    private static final Logger LOG = LoggerFactory.getLogger(PreSijoitteluProcessorLahtotilanteenHash.class);
     @Override
     public String name() {
         return getClass().getSimpleName();
@@ -27,6 +30,9 @@ public class PreSijoitteluProcessorLahtotilanteenHash implements PreSijoitteluPr
         sijoitteluajoWrapper.getHakukohteet().stream()
                 .flatMap(h -> h.getValintatapajonot().stream())
                 .flatMap(v -> v.getHakemukset().stream())
-                .forEach(h -> h.setLahtotilanteenHash(h.luoHash()));
+                .forEach(h -> {
+                    h.setLahtotilanteenHash(h.luoHash());
+                    //LOG.error("## {} {}", h.getHakemus().getHakemusOid(), h.getLahtotilanteenHash());
+                });
     }
 }
