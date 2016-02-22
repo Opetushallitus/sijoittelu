be098fda38657d8d8f7980f15e721273919464f5
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2016-02-22 13:30:34 +0200

    Change comment

--- sijoittelu-algoritmi-domain/src/main/java/fi/vm/sade/sijoittelu/domain/ValintatuloksenTila.java
+++ sijoittelu-algoritmi-domain/src/main/java/fi/vm/sade/sijoittelu/domain/ValintatuloksenTila.java
@@ -4,7 +4,7 @@ public enum ValintatuloksenTila {
     EI_VASTAANOTETTU_MAARA_AIKANA, // Hakija ei ole ilmoittanut paikkaa vastaanotetuksi maaraaikana ja on nain ollen hylatty
     PERUNUT,                       // Hakija ei ota paikkaa vastaan
     PERUUTETTU,                    // Hakijan tila on peruutettu
-    EHDOLLISESTI_VASTAANOTTANUT,    // Ehdollisesti vastaanottanut
-    VASTAANOTTANUT_SITOVASTI,       // Sitovasti vastaanottanut, kk-tila
+    EHDOLLISESTI_VASTAANOTTANUT,   // Ehdollisesti vastaanottanut
+    VASTAANOTTANUT_SITOVASTI,      // Sitovasti vastaanottanut
     KESKEN
 }
