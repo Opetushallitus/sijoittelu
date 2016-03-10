3c70c80865c06ae4ce01ecbe729a9f390d20f02b
Jani Rahkola <jani.rahkola@reaktor.com>
2016-03-10 15:13:04 +0200

    Throw on inconsistent Valintatulos data

--- sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/processors/PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/processors/PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku.java
@@ -6,13 +6,12 @@ import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWr
 import fi.vm.sade.sijoittelu.domain.ValintatuloksenTila;
 import fi.vm.sade.sijoittelu.domain.Valintatulos;
 import fi.vm.sade.sijoittelu.laskenta.external.resource.ValintaTulosServiceResource;
-import org.apache.commons.collections4.keyvalue.MultiKey;
+import org.apache.commons.collections4.map.MultiKeyMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku implements PreSijoitteluProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku.class);
@@ -25,13 +24,37 @@ public class PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku implem
     @Override
     public void process(SijoitteluajoWrapper sijoitteluajoWrapper) {
         LOG.info("(hakuOid={}) Haetaan valinnantilat Valinta-tulos-service -palvelusta", sijoitteluajoWrapper.getSijoitteluajo().getHakuOid());
-        Map<MultiKey, List<Valintatulos>> valintatulokset = valintaTulosServiceResource.valintatuloksetValinnantilalla(sijoitteluajoWrapper.getSijoitteluajo().getHakuOid()).stream()
-                .collect(Collectors.groupingBy(o -> new MultiKey<>(o.getValintatapajonoOid(), o.getHakemusOid(), o.getHakukohdeOid())));
-        sijoitteluajoWrapper.getHakukohteet().stream().flatMap(HakukohdeWrapper::hakukohteenHakijat).flatMap(h -> h.getValintatulos().stream()).map(v -> {
-            ValintatuloksenTila valintatuloksenTila = valintatulokset.get(new MultiKey<>(v.getValintatapajonoOid(), v.getHakemusOid(), v.getHakukohdeOid())).stream().
-                map(Valintatulos::getTila).findAny().orElse(ValintatuloksenTila.KESKEN);
-            v.setTila(valintatuloksenTila, "");
-            return v;
+        MultiKeyMap<String, Valintatulos> vastaanottotiedot = indexValintatulokset(valintaTulosServiceResource.valintatuloksetValinnantilalla(sijoitteluajoWrapper.getSijoitteluajo().getHakuOid()));
+        valintatulokset(sijoitteluajoWrapper).forEach(v -> {
+            ValintatuloksenTila tila = ValintatuloksenTila.KESKEN;
+            String jonoOid = v.getValintatapajonoOid();
+            String hakemusOid = v.getHakemusOid();
+            String hakukohdeOid = v.getHakukohdeOid();
+            if (vastaanottotiedot.containsKey(jonoOid, hakemusOid, hakukohdeOid)) {
+                tila = vastaanottotiedot.get(jonoOid, hakemusOid, hakukohdeOid).getTila();
+                if (null == tila) {
+                    throw new IllegalStateException(String.format("Hakemus: %s, hakukohde: %s, jono: %s vastaanottotieto valinta-tulos-servicestä on null", hakemusOid, hakukohdeOid, jonoOid));
+                }
+            }
+            v.setTila(tila, "");
         });
+    }
+
+    private Stream<Valintatulos> valintatulokset(SijoitteluajoWrapper sijoitteluajoWrapper) {
+        return sijoitteluajoWrapper.getHakukohteet().stream().flatMap(HakukohdeWrapper::hakukohteenHakijat).flatMap(h -> h.getValintatulos().stream());
+    }
+
+    private MultiKeyMap<String, Valintatulos> indexValintatulokset(List<Valintatulos> valintatulokset) {
+        MultiKeyMap<String, Valintatulos> vs = new MultiKeyMap<>();
+        valintatulokset.stream().forEach(v -> {
+            String jonoOid = v.getValintatapajonoOid();
+            String hakemusOid = v.getHakemusOid();
+            String hakukohdeOid = v.getHakukohdeOid();
+            if (vs.containsKey(jonoOid, hakemusOid, hakukohdeOid)) {
+                throw new IllegalStateException(String.format("Hakemus: %s, hakukohde: %s, jono: %s useita valintatuloksia", hakemusOid, hakukohdeOid, jonoOid));
+            }
+            vs.put(jonoOid, hakemusOid, hakukohdeOid, v);
+        });
+        return vs;
     }
 }
