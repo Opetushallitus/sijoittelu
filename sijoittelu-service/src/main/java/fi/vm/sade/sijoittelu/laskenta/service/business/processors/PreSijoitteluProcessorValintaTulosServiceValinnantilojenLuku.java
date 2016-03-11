6c549aa09e963f262ea1a1af6189e4c3716546fc
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-03-11 12:46:06 +0200

    Simplify
    No functional changes

--- sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/processors/PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/processors/PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku.java
@@ -1,7 +1,7 @@
 package fi.vm.sade.sijoittelu.laskenta.service.business.processors;
 
-import com.google.common.collect.MultimapBuilder;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.presijoitteluprocessor.PreSijoitteluProcessor;
+import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.HakukohdeWrapper;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
 import fi.vm.sade.sijoittelu.domain.ValintatuloksenTila;
 import fi.vm.sade.sijoittelu.domain.Valintatulos;
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku implements PreSijoitteluProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku.class);
@@ -27,9 +26,10 @@ public class PreSijoitteluProcessorValintaTulosServiceValinnantilojenLuku implem
     public void process(SijoitteluajoWrapper sijoitteluajoWrapper) {
         LOG.info("(hakuOid={}) Haetaan valinnantilat Valinta-tulos-service -palvelusta", sijoitteluajoWrapper.getSijoitteluajo().getHakuOid());
         Map<MultiKey, List<Valintatulos>> valintatulokset = valintaTulosServiceResource.valintatuloksetValinnantilalla(sijoitteluajoWrapper.getSijoitteluajo().getHakuOid()).stream()
-                .collect(Collectors.groupingBy(o -> new MultiKey(o.getValintatapajonoOid(), o.getHakemusOid(), o.getHakukohdeOid())));
-        sijoitteluajoWrapper.getHakukohteet().stream().flatMap(h -> h.hakukohteenHakijat()).flatMap(h -> h.getValintatulos().stream()).map(v -> {
-            ValintatuloksenTila valintatuloksenTila = valintatulokset.get(new MultiKey(v.getValintatapajonoOid(), v.getHakemusOid(), v.getHakukohdeOid())).stream().map(vv -> vv.getTila()).findAny().orElse(ValintatuloksenTila.KESKEN);
+                .collect(Collectors.groupingBy(o -> new MultiKey<>(o.getValintatapajonoOid(), o.getHakemusOid(), o.getHakukohdeOid())));
+        sijoitteluajoWrapper.getHakukohteet().stream().flatMap(HakukohdeWrapper::hakukohteenHakijat).flatMap(h -> h.getValintatulos().stream()).map(v -> {
+            ValintatuloksenTila valintatuloksenTila = valintatulokset.get(new MultiKey<>(v.getValintatapajonoOid(), v.getHakemusOid(), v.getHakukohdeOid())).stream().
+                map(Valintatulos::getTila).findAny().orElse(ValintatuloksenTila.KESKEN);
             v.setTila(valintatuloksenTila, "");
             return v;
         });
