acdac0609839206d7c630869f7a86a270b94fce8
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-04-28 16:18:49 +0300

    Parse vastaanotto actions too

--- sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/external/resource/dto/VastaanottoDTO.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/external/resource/dto/VastaanottoDTO.java
@@ -1,12 +1,12 @@
 package fi.vm.sade.sijoittelu.laskenta.external.resource.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import fi.vm.sade.sijoittelu.domain.ValintatuloksenTila;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class VastaanottoDTO {
     private String henkiloOid;
     private String hakukohdeOid;
+    private String action;
 
     public VastaanottoDTO() { }
 
@@ -24,5 +24,28 @@ public class VastaanottoDTO {
 
     public void setHakukohdeOid(String hakukohdeOid) {
         this.hakukohdeOid = hakukohdeOid;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public VastaanottoType typeOfVastaanotto() {
+        return VastaanottoType.valueOf(action);
+    }
+
+    public enum VastaanottoType {
+        VastaanotaSitovasti(false),
+        VastaanotaEhdollisesti(true);
+
+        public final boolean ehdollinen;
+
+        VastaanottoType(boolean ehdollinen) {
+            this.ehdollinen = ehdollinen;
+        }
     }
 }
