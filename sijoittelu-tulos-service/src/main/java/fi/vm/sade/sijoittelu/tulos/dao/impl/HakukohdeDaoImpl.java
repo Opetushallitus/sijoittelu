f7cba864c38f74b704df56740b574018f26e0e38
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2016-06-28 20:36:34 +0300

    Revert "Optimize haeHakukohteetJoihinHakemusOsallistuu"
    This reverts commit 585467fb8053431d84317eb4837e4a7b598b99a8.

--- sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/dao/impl/HakukohdeDaoImpl.java
+++ sijoittelu-tulos-service/src/main/java/fi/vm/sade/sijoittelu/tulos/dao/impl/HakukohdeDaoImpl.java
@@ -240,20 +240,22 @@ public class HakukohdeDaoImpl implements HakukohdeDao {
 
     @Override
     public List<Hakukohde> haeHakukohteetJoihinHakemusOsallistuu(Long sijoitteluajoId, String hakemusOid) {
-        List<Hakukohde> hakukohteet = morphiaDS.find(Hakukohde.class)
-                .field("sijoitteluajoId").equal(sijoitteluajoId)
-                .field("valintatapajonot.hakemukset.hakemusOid").equal(hakemusOid)
-                .enableSnapshotMode()
-                .asList();
-        hakukohteet.forEach(hakukohde -> {
-            hakukohde.getValintatapajonot().forEach(valintatapajono -> {
-                valintatapajono.setHakemukset(
-                        valintatapajono.getHakemukset().stream()
-                                .filter(hakemus -> hakemusOid.equals(hakemus.getHakemusOid()))
-                                .collect(Collectors.toList()));
-            });
-        });
-        return hakukohteet;
+        final BasicDBObject query = new BasicDBObject("sijoitteluajoId", sijoitteluajoId).append("valintatapajonot.hakemukset.hakemusOid", hakemusOid);
+        String map = MongoMapReduceUtil.shallowCloneJs + " copy.valintatapajonot = copy.valintatapajonot.map(function(jono) { jono = shallowClone(jono); jono.hakemukset = (jono.hakemukset || []).filter(function (hakemus) { return hakemus.hakemusOid == '"+hakemusOid+"' }); return jono }); emit(this.oid, copy) }\n";
+        String reduce = "function(key, values) { return values[0] }";
+        DBCollection collection = morphiaDS.getCollection(Hakukohde.class);
+        MapReduceCommand cmd = new MapReduceCommand(
+            collection,
+            map,
+            reduce,
+            null,
+            MapReduceCommand.OutputType.INLINE,
+            query);
+        MapReduceOutput out = collection.mapReduce(cmd);
+
+        return StreamSupport.stream(out.results().spliterator(), false)
+                .map(dbObject -> new Mapper().fromDBObject(Hakukohde.class, (DBObject) dbObject.get("value"), new DefaultEntityCache()))
+                .collect(Collectors.toList());
     }
 
     @Override
