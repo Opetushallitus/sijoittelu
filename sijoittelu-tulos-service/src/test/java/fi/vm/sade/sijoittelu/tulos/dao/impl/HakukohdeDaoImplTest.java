6e7743129a8593b4bea74f1850b703a7b2d9fc1e
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-08-30 12:15:41 +0300

    BUG-1076 assert "No throw expected"

--- sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/dao/impl/HakukohdeDaoImplTest.java
+++ sijoittelu-tulos-service/src/test/java/fi/vm/sade/sijoittelu/tulos/dao/impl/HakukohdeDaoImplTest.java
@@ -4,6 +4,9 @@ import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
 import fi.vm.sade.sijoittelu.tulos.dao.HakukohdeDao;
+import fi.vm.sade.sijoittelu.tulos.dto.KevytHakukohdeDTO;
+
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Iterator;
 
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 
@@ -29,7 +34,7 @@ public class HakukohdeDaoImplTest {
     @Test
     @UsingDataSet(locations = {"/fi/vm/sade/sijoittelu/tulos/resource/sijoittelu-basedata.json", "/fi/vm/sade/sijoittelu/tulos/resource/hyvaksytty-ilmoitettu.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void testKevytHakukohdeFetch() {
-
-        hakukohdeDao.getHakukohdeForSijoitteluajoIterator(1409055160621L, "1.2.246.562.5.72607738902");
+        Iterator<KevytHakukohdeDTO> kevyetHakukohteet = hakukohdeDao.getHakukohdeForSijoitteluajoIterator(1409055160621L, "1.2.246.562.5.72607738902");
+        Assert.assertFalse("No throw expected", kevyetHakukohteet.hasNext());
     }
 }
