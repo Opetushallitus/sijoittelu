44c768f834137b25ca5ba595a78be1ddc41dba85
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-10-20 14:04:48 +0300

    BUG-1064 unit test where problem arise

--- sijoittelu-algoritmi-batch/src/test/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/spec/HakijaryhmaSpecTest.java
+++ sijoittelu-algoritmi-batch/src/test/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/spec/HakijaryhmaSpecTest.java
@@ -1,8 +1,21 @@
 package fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.spec;
 
-import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.SijoitteluTestSpec;
+import com.google.common.collect.Lists;
+import fi.vm.sade.sijoittelu.batch.logic.impl.DomainConverter;
+import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.*;
+import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.util.SijoitteluAlgorithmUtil;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
+import fi.vm.sade.sijoittelu.domain.*;
+import fi.vm.sade.valintalaskenta.domain.dto.HakukohdeDTO;
+import fi.vm.sade.valintalaskenta.domain.dto.valintatieto.HakuDTO;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.TestHelper.*;
 
 /**
@@ -10,6 +23,31 @@ import static fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.TestHelper.*;
  */
 public class HakijaryhmaSpecTest extends SijoitteluTestSpec {
 
+    @Test
+    public void testaaVastaanottoSiirtyyYlemmalleHakukohteelleVaikkaHakijaOnSinneVaralla() {
+        HakuDTO haku = TestHelper.readHakuDTOFromJson("testdata_erikoistapaukset/sijoittelu_vastaanotto_siirtyy_ylemmalle_vaikka_hakija_varasijalla.json");
+        Valintatulos valintatulos = new Valintatulos();
+        valintatulos.setHakemusOid("1.2.246.562.24.00000000003","");
+        valintatulos.setHakukohdeOid("1.2.246.562.11.00000000001","");
+        valintatulos.setValintatapajonoOid("tkk_jono_2","");
+        valintatulos.setJulkaistavissa(true, "");
+        valintatulos.setTila(ValintatuloksenTila.EHDOLLISESTI_VASTAANOTTANUT, "");
+
+        Valintatulos kesken = new Valintatulos();
+        kesken.setHakemusOid("1.2.246.562.24.00000000003","");
+        kesken.setHakukohdeOid("1.2.246.562.11.00000000002","");
+        kesken.setValintatapajonoOid("tkk_jono_1","");
+        kesken.setTila(ValintatuloksenTila.KESKEN, "");
+
+        List<Valintatulos> valintatulokset = Arrays.asList(valintatulos, kesken);
+        List<Hakukohde> hakukohteet = haku.getHakukohteet().parallelStream().map(DomainConverter::convertToHakukohde).collect(Collectors.toList());
+
+        Hakukohde ensimmainenHk = hakukohteet.stream().filter(h -> h.getOid().endsWith("1")).findAny().get();
+        ensimmainenHk.getValintatapajonot().iterator().next().getHakemukset().iterator().next().setEdellinenTila(HakemuksenTila.HYVAKSYTTY);
+
+        final SijoitteluajoWrapper sijoitteluAjo = SijoitteluajoWrapperFactory.createSijoitteluAjoWrapper(new SijoitteluAjo(), hakukohteet, valintatulokset, Collections.emptyMap());
+        SijoittelunTila s = SijoitteluAlgorithmUtil.sijoittele(sijoitteluAjo);
+    }
 
     @Test
     public void testaaHakijaryhmaYlitaytonRajatapaukset() {
