710e6bd125c84d1fcf6fd3f88f3630dc02f607e1
Heikki Honkanen <heikki.honkanen@gofore.com>
2016-11-30 11:38:48 +0200

    Add saveSijoitteluToValintarekisteri property to SijoitteluServiceJetty.

--- sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluServiceJetty.java
+++ sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluServiceJetty.java
@@ -61,6 +61,7 @@ public class SijoitteluServiceJetty {
         System.setProperty("sijoittelu-service.mongodb.dbname", Optional.ofNullable(System.getProperty("sijoitteludbName")).orElse("sijoitteludb"));
         System.setProperty("sijoittelu-service.mongodb.uri", System.getProperty("sijoitteluMongoUri"));
         System.setProperty("sijoittelu-service.hakukohdeDao.batchSize", "300");
+        System.setProperty("sijoittelu-service.saveSijoitteluToValintarekisteri", "true");
         System.setProperty("omatsivut.email.application.modify.link.en", "https://en.test.domain/token/");
         System.setProperty("omatsivut.email.application.modify.link.fi", "https://fi.test.domain/token/");
         System.setProperty("omatsivut.email.application.modify.link.sv", "https://sv.test.domain/token/");
