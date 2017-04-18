defdd9636079d9f05076783ffa252c4033b9be1c
Jani Rahkola <jani.rahkola@reaktor.com>
2017-04-18 16:59:17 +0300

    Testi hyväksyttyä ylempien peruunnutukselle

--- sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluBusinessTest.java
+++ sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/SijoitteluBusinessTest.java
@@ -1,6 +1,5 @@
 package fi.vm.sade.sijoittelu;
 
-import com.google.common.collect.Sets;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
@@ -8,26 +7,20 @@ import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
 import fi.vm.sade.sijoittelu.batch.logic.impl.DomainConverter;
 import fi.vm.sade.sijoittelu.domain.*;
-import fi.vm.sade.sijoittelu.laskenta.external.resource.HakuV1Resource;
-import fi.vm.sade.sijoittelu.laskenta.external.resource.OhjausparametriResource;
 import fi.vm.sade.sijoittelu.laskenta.external.resource.dto.ParametriDTO;
-import fi.vm.sade.sijoittelu.laskenta.external.resource.dto.ResultHakuDTO;
 import fi.vm.sade.sijoittelu.laskenta.service.business.SijoitteluBusinessService;
+import fi.vm.sade.sijoittelu.laskenta.service.business.ValintatulosWithVastaanotto;
 import fi.vm.sade.sijoittelu.laskenta.service.it.TarjontaIntegrationService;
-import fi.vm.sade.sijoittelu.laskenta.util.HakuUtil;
 import fi.vm.sade.sijoittelu.tulos.dao.HakukohdeDao;
 import fi.vm.sade.sijoittelu.tulos.dao.SijoitteluDao;
 import fi.vm.sade.sijoittelu.tulos.dao.ValiSijoitteluDao;
 import fi.vm.sade.sijoittelu.tulos.dao.ValintatulosDao;
 
-import fi.vm.sade.valintalaskenta.domain.dto.HakukohdeDTO;
-import fi.vm.sade.valintalaskenta.domain.dto.ValinnanvaiheDTO;
 import fi.vm.sade.valintalaskenta.domain.dto.ValintatapajonoDTO;
 import fi.vm.sade.valintalaskenta.domain.dto.valintatieto.HakuDTO;
 import fi.vm.sade.valintalaskenta.tulos.service.impl.ValintatietoService;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -50,7 +43,6 @@ import static com.google.common.collect.Sets.newHashSet;
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -82,6 +74,8 @@ public class SijoitteluBusinessTest {
     @Autowired
     private TarjontaIntegrationService tarjontaIntegrationService;
 
+    private ValintatulosWithVastaanotto valintatulosWithVastaanotto;
+
     @Autowired
     private ValintatietoService valintatietoService;
 
@@ -97,10 +91,14 @@ public class SijoitteluBusinessTest {
     @Before
     public void setup() {
         tarjontaIntegrationService = mock(TarjontaIntegrationService.class);
+        valintatulosWithVastaanotto = mock(ValintatulosWithVastaanotto.class);
 
         ReflectionTestUtils.setField(sijoitteluService,
                 "tarjontaIntegrationService",
                 tarjontaIntegrationService);
+        ReflectionTestUtils.setField(sijoitteluService,
+                "valintatulosWithVastaanotto",
+                valintatulosWithVastaanotto);
 
         fi.vm.sade.sijoittelu.laskenta.external.resource.dto.HakuDTO hakuDto = new fi.vm.sade.sijoittelu.laskenta.external.resource.dto.HakuDTO();
         hakuDto.setKohdejoukkoUri("haunkohdejoukko_11#1");
@@ -251,6 +249,69 @@ public class SijoitteluBusinessTest {
         thrown.expectMessage("Edellisessä sijoittelussa olleet jonot [jono1] puuttuvat sijoittelusta, vaikka ne ovat valintaperusteissa yhä aktiivisina");
 
         sijoitteluService.sijoittele(haku, newHashSet("jono1", "jono2", "jono3"), newHashSet("jono1", "jono2", "jono3"));
+    }
+
+    @Test()
+    @UsingDataSet(locations = "peruuta_alemmat.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void testPeruunnaHyvaksyttyaYlemmatHakutoiveetKunAMKOPEHaku() {
+        fi.vm.sade.sijoittelu.laskenta.external.resource.dto.HakuDTO tarjontaHaku = new fi.vm.sade.sijoittelu.laskenta.external.resource.dto.HakuDTO();
+        tarjontaHaku.setKohdejoukkoUri("haunkohdejoukko_12" + "#1");
+        tarjontaHaku.setKohdejoukonTarkenne("haunkohdejoukontarkenne_2#1");
+        when(tarjontaIntegrationService.getHakuByHakuOid(anyString())).thenReturn(tarjontaHaku);
+        HakuDTO haku = valintatietoService.haeValintatiedot("haku1");
+        sijoitteluService.sijoittele(haku, newHashSet("jono1", "jono2", "jono3"), newHashSet("jono1", "jono2", "jono3"));
+        sijoitteluService.sijoittele(haku, newHashSet("jono1", "jono2", "jono3"), newHashSet("jono1", "jono2", "jono3"));
+
+        List<Hakukohde> hakukohteet = hakukohdeDao.getHakukohdeForSijoitteluajo(sijoitteluDao.getLatestSijoitteluajo("haku1").get().getSijoitteluajoId());
+        Hakemus hakija3YlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde1")).findAny().get().getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija3")).findAny().get();
+        Hakemus hakija3AlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde2")).findAny().get()
+                .getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija3")).findAny().get();
+        Hakemus hakija4YlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde1")).findAny().get().getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija4")).findAny().get();
+        Hakemus hakija4AlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde2")).findAny().get()
+                .getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija4")).findAny().get();
+        assertEquals(HakemuksenTila.HYVAKSYTTY, hakija3AlempiToive.getTila());
+        assertEquals(HakemuksenTila.PERUUNTUNUT, hakija3YlempiToive.getTila());
+        assertEquals(HakemuksenTila.HYVAKSYTTY, hakija4AlempiToive.getTila());
+        assertEquals(HakemuksenTila.PERUUNTUNUT, hakija4YlempiToive.getTila());
+
+        Valintatulos peruYlempiHakija = new Valintatulos("jono1", "hakija2", "hakukohde1", "1.2.246.562.24.42438870792", "haku1", 0);
+        peruYlempiHakija.setJulkaistavissa(true, "");
+        peruYlempiHakija.setTila(ValintatuloksenTila.EI_VASTAANOTETTU_MAARA_AIKANA, "");
+
+        Valintatulos julkaiseHakijan3AlempiToive = new Valintatulos("jono2", "hakija3", "hakukohde2", "1.2.246.562.24.45661259022", "haku1", 1);
+        julkaiseHakijan3AlempiToive.setJulkaistavissa(true, "");
+        julkaiseHakijan3AlempiToive.setTila(ValintatuloksenTila.KESKEN, "");
+
+        when(valintatulosWithVastaanotto.forHaku("haku1")).thenReturn(Arrays.asList(peruYlempiHakija, julkaiseHakijan3AlempiToive));
+        sijoitteluService.sijoittele(haku, newHashSet("jono1", "jono2", "jono3"), newHashSet("jono1", "jono2", "jono3"));
+
+        hakukohteet = hakukohdeDao.getHakukohdeForSijoitteluajo(sijoitteluDao.getLatestSijoitteluajo("haku1").get().getSijoitteluajoId());
+        hakija3YlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde1")).findAny().get().getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija3")).findAny().get();
+        hakija3AlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde2")).findAny().get()
+                .getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija3")).findAny().get();
+        hakija4YlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde1")).findAny().get().getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija4")).findAny().get();
+        hakija4AlempiToive = hakukohteet.stream()
+                .filter(h -> h.getOid().equals("hakukohde2")).findAny().get()
+                .getValintatapajonot().get(0)
+                .getHakemukset().stream().filter(h -> h.getHakemusOid().equals("hakija4")).findAny().get();
+        assertEquals(HakemuksenTila.HYVAKSYTTY, hakija3AlempiToive.getTila());
+        assertEquals(HakemuksenTila.PERUUNTUNUT, hakija3YlempiToive.getTila());
+        assertEquals(HakemuksenTila.PERUUNTUNUT, hakija4AlempiToive.getTila());
+        assertEquals(HakemuksenTila.HYVAKSYTTY, hakija4YlempiToive.getTila());
     }
 
     @Test
