b2be412ec011368fc48a3dadcc045a8a64db1c03
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-12-15 14:03:34 +0200

    BUG-1581 : Std lib format instead of log4j
    IDEs don't catch parameter mismatches in the log4j
    specific formatting as easily as String.format.

--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/postsijoitteluprocessor/PostSijoitteluProcessorPeruunnutaYlijaaneetVarallaTilaisetJosVarasijatayttoOnPaattynyt.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/postsijoitteluprocessor/PostSijoitteluProcessorPeruunnutaYlijaaneetVarallaTilaisetJosVarasijatayttoOnPaattynyt.java
@@ -20,7 +20,7 @@ public class PostSijoitteluProcessorPeruunnutaYlijaaneetVarallaTilaisetJosVarasi
     public void process(SijoitteluajoWrapper sijoitteluajoWrapper) {
 
         if (sijoitteluajoWrapper.varasijaSaannotVoimassa()) {
-            LOG.info("Varasijasäännöt ovat voimassa, varmistetaan että " + VARALLA + "-tilaiset muutetaan tilaan " + PERUUNTUNUT + " kaikille jonoille, joiden varasijatäyttö on päättynyt");
+            LOG.info(String.format("Varasijasäännöt ovat voimassa, varmistetaan että %s-tilaiset muutetaan tilaan %s kaikille jonoille, joiden varasijatäyttö on päättynyt", VARALLA, PERUUNTUNUT));
             List<ValintatapajonoWrapper> kasiteltavatJonot = sijoitteluajoWrapper.getHakukohteet().stream()
                 .flatMap(hkv -> hkv.getValintatapajonot().stream())
                 .filter(sijoitteluajoWrapper::onkoVarasijaSaannotVoimassaJaVarasijaTayttoPaattynyt)
@@ -30,12 +30,10 @@ public class PostSijoitteluProcessorPeruunnutaYlijaaneetVarallaTilaisetJosVarasi
                 .filter(HakemusWrapper::isVaralla)
                 .forEach(hw -> {
                     if (hw.isTilaVoidaanVaihtaa()) {
-                        LOG.info("(AjoId: {} ) PostProcessor: Asetetaan " + VARALLA + " oleva hakemus {} tilaan " + PERUUNTUNUT,
-                            sijoitteluajoWrapper.getSijoitteluAjoId(), hw.getHakemus().getHakemusOid());
+                        LOG.info(String.format("(AjoId: %s ) PostProcessor: Asetetaan %s oleva hakemus %s tilaan %s", sijoitteluajoWrapper.getSijoitteluAjoId(), VARALLA, hw.getHakemus().getHakemusOid(), PERUUNTUNUT));
                         TilojenMuokkaus.asetaTilaksiPeruuntunutHakukierrosPaattynyt(hw);
                     } else {
-                        LOG.info("(AjoId: {} ) PostProcessor: Oltaisiin haluttu asettaa " + VARALLA + " oleva hakemus {} tilaan " + PERUUNTUNUT + ", mutta sen tila ei ole jostain syystä muokattavissa",
-                            sijoitteluajoWrapper.getSijoitteluAjoId(), hw.getHakemus().getHakemusOid());
+                        LOG.info(String.format("(AjoId: %s ) PostProcessor: Oltaisiin haluttu asettaa %s oleva hakemus %s tilaan %s, mutta sen tila ei ole jostain syystä muokattavissa", sijoitteluajoWrapper.getSijoitteluAjoId(), VARALLA, hw.getHakemus().getHakemusOid(), PERUUNTUNUT));
                     }
                 });
         }
