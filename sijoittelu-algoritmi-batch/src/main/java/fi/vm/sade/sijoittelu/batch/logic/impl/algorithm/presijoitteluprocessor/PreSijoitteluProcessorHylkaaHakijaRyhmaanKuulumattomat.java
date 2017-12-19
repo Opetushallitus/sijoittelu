c36b57bc6388f7ba752f3ea287249050a9462e68
Mikko Siukola <mikko.siukola@cybercom.com>
2017-12-19 14:12:46 +0200

    Sijoittelu kaatumaan jos vain ryhmään kuuluvat hyväksytään-valintaryhmiä on useampia kuin yksi

--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorHylkaaHakijaRyhmaanKuulumattomat.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorHylkaaHakijaRyhmaanKuulumattomat.java
@@ -14,38 +14,42 @@ public class PreSijoitteluProcessorHylkaaHakijaRyhmaanKuulumattomat implements P
     @Override
     public void process(SijoitteluajoWrapper sijoitteluajoWrapper) {
         this.hylkaaHakijaryhmiinKuulumattomat(sijoitteluajoWrapper);
-
     }
 
     private void hylkaaHakijaryhmiinKuulumattomat(SijoitteluajoWrapper sijoitteluajoWrapper) {
         sijoitteluajoWrapper.getHakukohteet().forEach(hakukohde -> {
-            List<HakijaryhmaWrapper> vainRyhmaanKuuluvatHyvaksytaan = hakukohde
-                    .getHakijaryhmaWrappers()
-                    .stream()
-                    .filter(h -> h.getHakijaryhma().isKaytaKaikki())
-                    .collect(Collectors.toList());
+            List<HakijaryhmaWrapper> ryhmatJoistaHyvaksytaanKaikki = hakukohde
+                .getHakijaryhmaWrappers()
+                .stream()
+                .filter(h -> h.getHakijaryhma().isKaytaKaikki())
+                .collect(Collectors.toList());
+            if (ryhmatJoistaHyvaksytaanKaikki.size() > 1) {
+                throw new IllegalStateException(String.format("(Sijoitteluajo %s) Hakukohteelle %s on olemassa useampia kuin yksi hakijaryhmä, " +
+                    "josta kaikki ryhmään kuuluvat olisi tarkoitus hyväksyä. Sijoittelua ei voida suorittaa.",
+                    sijoitteluajoWrapper.getSijoitteluAjoId(),
+                    hakukohde.getHakukohde().getOid()));
+            }
             // Hylätään hakijat, jotka eivät kuulu hakijaryhmään
-            vainRyhmaanKuuluvatHyvaksytaan.forEach(h -> {
-                String jonoOid = h.getHakijaryhma().getValintatapajonoOid();
+            ryhmatJoistaHyvaksytaanKaikki.forEach(ryhmaWrapper -> {
+                String jonoOid = ryhmaWrapper.getHakijaryhma().getValintatapajonoOid();
                 if (jonoOid != null) {
                     hakukohde.getValintatapajonot()
-                            .stream()
-                            .filter(v -> v.getValintatapajono().getOid().equals(jonoOid))
-                            .forEach(v -> hylkaaRyhmaanKuulumattomat(h, v));
+                        .stream()
+                        .filter(v -> v.getValintatapajono().getOid().equals(jonoOid))
+                        .forEach(v -> hylkaaRyhmaanKuulumattomat(ryhmaWrapper, v));
                 } else {
-                    hakukohde.getValintatapajonot().forEach(v -> hylkaaRyhmaanKuulumattomat(h, v));
+                    hakukohde.getValintatapajonot().forEach(v -> hylkaaRyhmaanKuulumattomat(ryhmaWrapper, v));
                 }
             });
         });
-
     }
 
-    private void hylkaaRyhmaanKuulumattomat(HakijaryhmaWrapper h, ValintatapajonoWrapper v) {
-        v.getHakemukset().forEach(hakemus -> {
-            if (!h.getHenkiloWrappers().contains(hakemus.getHenkilo()) && hakemus.isTilaVoidaanVaihtaa()) {
-                hakemus.setTilaVoidaanVaihtaa(false);
+    private void hylkaaRyhmaanKuulumattomat(HakijaryhmaWrapper ryhmaWrapper, ValintatapajonoWrapper jonoWrapper) {
+        jonoWrapper.getHakemukset().forEach(hakemus -> {
+            if (!ryhmaWrapper.getHenkiloWrappers().contains(hakemus.getHenkilo()) && hakemus.isTilaVoidaanVaihtaa()) {
                 hakemus.getHakemus().setTila(HakemuksenTila.HYLATTY);
-                hakemus.getHakemus().setTilanKuvaukset(TilanKuvaukset.hylattyHakijaryhmaanKuulumattomana(h.getHakijaryhma().getNimi()));
+                hakemus.getHakemus().setTilanKuvaukset(TilanKuvaukset.hylattyHakijaryhmaanKuulumattomana(ryhmaWrapper.getHakijaryhma().getNimi()));
+                hakemus.setTilaVoidaanVaihtaa(false);
             }
         });
     }
