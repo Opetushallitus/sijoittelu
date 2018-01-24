0766a00bbedc91501f80dd8a002d3914b1a43b4f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-01-24 12:51:44 +0200

    Improve error logging.
    This way it will be easier to fix conflicting data.

--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorHylkaaHakijaRyhmaanKuulumattomat.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorHylkaaHakijaRyhmaanKuulumattomat.java
@@ -5,6 +5,7 @@ import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.HakijaryhmaWrap
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.ValintatapajonoWrapper;
 import fi.vm.sade.sijoittelu.domain.HakemuksenTila;
+import fi.vm.sade.sijoittelu.domain.Hakijaryhma;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,10 +25,20 @@ public class PreSijoitteluProcessorHylkaaHakijaRyhmaanKuulumattomat implements P
                 .filter(h -> h.getHakijaryhma().isKaytaKaikki())
                 .collect(Collectors.toList());
             if (ryhmatJoistaVainKuuluvatHyvaksytaan.size() > 1) {
+                List<String> ryhmienOiditJaNimet = ryhmatJoistaVainKuuluvatHyvaksytaan.stream().map(hjr ->
+                {
+                    Hakijaryhma hakijaryhma = hjr.getHakijaryhma();
+                    return String.format("%s (\"%s\"), hakukohdeOid= %s , valintatapajonoOid = %s .",
+                        hakijaryhma.getOid(),
+                        hakijaryhma.getNimi(),
+                        hakijaryhma.getHakukohdeOid(),
+                        hakijaryhma.getValintatapajonoOid());
+                }).collect(Collectors.toList());
                 throw new IllegalStateException(String.format("(Sijoitteluajo %s) Hakukohteelle %s on olemassa useampi kuin yksi hakijaryhmä, " +
-                    "josta vain ryhmään kuuluvat olisi tarkoitus hyväksyä. Sijoittelua ei voida suorittaa.",
+                    "josta vain ryhmään kuuluvat olisi tarkoitus hyväksyä. Sijoittelua ei voida suorittaa. Ryhmät: %s",
                     sijoitteluajoWrapper.getSijoitteluAjoId(),
-                    hakukohde.getHakukohde().getOid()));
+                    hakukohde.getHakukohde().getOid(),
+                    ryhmienOiditJaNimet));
             }
             // Hylätään hakijat, jotka eivät kuulu hakijaryhmään
             ryhmatJoistaVainKuuluvatHyvaksytaan.forEach(ryhmaWrapper -> {
