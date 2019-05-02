1b8f647b4009b8ef4729203dcdd077a136d58967
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-05-02 12:44:29 +0300

    Remove timing
    It just traverses a relatively small data structure in
    memory and manipulates it a bit, not taking more than
    microseconds, which means that the timing data is not
    interesting.

--- sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/postsijoitteluprocessor/PostSijoitteluProcessorAsetaSivssnov.java
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/postsijoitteluprocessor/PostSijoitteluProcessorAsetaSivssnov.java
@@ -1,25 +1,25 @@
 package fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.postsijoitteluprocessor;
 
-import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.util.Timer;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
 import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.ValintatapajonoWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class PostSijoitteluProcessorAsetaSivssnov implements PostSijoitteluProcessor {
+    private static final Logger LOG = LoggerFactory.getLogger(PostSijoitteluProcessorAsetaSivssnov.class);
 
     @Override
     public void process(SijoitteluajoWrapper sijoitteluajoWrapper) {
         if (sijoitteluajoWrapper.varasijaSaannotVoimassa()) {
             String hakuOid = sijoitteluajoWrapper.getSijoitteluajo().getHakuOid();
 
-            Timer timer = Timer.start("Post-processor Aseta Sivssnov", "haulle " + hakuOid, PostSijoitteluProcessorAsetaSivssnov.class);
-
             sijoitteluajoWrapper.getHakukohteet().stream()
                     .flatMap(hkv -> hkv.getValintatapajonot().stream())
                     .map(ValintatapajonoWrapper::getValintatapajono)
                     .forEach(vtj -> vtj.setSijoiteltuIlmanVarasijasaantojaNiidenOllessaVoimassa(true));
 
-            timer.stop("Haulle " + hakuOid);
+            LOG.info(String.format("Asetettiin \"sijoiteltu ilman varasijasääntöjä niiden ollessa voimassa\" haun %s valintatapajonoille.", hakuOid));
         }
     }
 }
