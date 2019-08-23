80ef1be9c23ff53c1085989a88552258ef11541f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-08-23 14:20:55 +0300

    VTKU-51 : Tarkista hakutoiveprioriteetit
    Saman hakemusten tuloksissa pitäisi olla sama
    hakutoiveprioriteetti saman hakukohteen sisällä.
    Pakottamalla tämä preprocessorissa voimme varmistua
    siitä, että testeissä käytetään realistisempaa
    testidataa (ja samalla varautua siihen, että joku
    saisi tuotannossa näin hullun tilanteen aikaan).

---
+++ sijoittelu-algoritmi-batch/src/main/java/fi/vm/sade/sijoittelu/batch/logic/impl/algorithm/presijoitteluprocessor/PreSijoitteluProcessorTarkistaHakutoiveidenPrioriteettienEheys.java
@@ -0,0 +1,44 @@
+package fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.presijoitteluprocessor;
+
+import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.HakemusWrapper;
+import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.HenkiloWrapper;
+import fi.vm.sade.sijoittelu.batch.logic.impl.algorithm.wrappers.SijoitteluajoWrapper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PreSijoitteluProcessorTarkistaHakutoiveidenPrioriteettienEheys implements PreSijoitteluProcessor {
+    @Override
+    public void process(SijoitteluajoWrapper sijoitteluajoWrapper) {
+        Set<HenkiloWrapper> kaikkiHakijat = sijoitteluajoWrapper.getHakukohteet().stream()
+            .flatMap(hk -> hk.getValintatapajonot().stream()
+                .flatMap(j -> j.getHakemukset().stream()
+                    .map(HakemusWrapper::getHenkilo)))
+            .collect(Collectors.toSet());
+
+        kaikkiHakijat.forEach(henkiloWrapper -> {
+            Map<String, List<HakemusWrapper>> hakemuksenTuloksetByHakukohdeOid = henkiloWrapper.getHakemukset().stream()
+                .collect(Collectors.groupingBy(HakemusWrapper::getHakukohdeOid));
+            hakemuksenTuloksetByHakukohdeOid.forEach(this::tarkistaEttaTuloksissaOnSamaHakutoivePrioriteetti);
+        });
+    }
+
+    private void tarkistaEttaTuloksissaOnSamaHakutoivePrioriteetti(String hakukohdeOid, List<HakemusWrapper> samanToiveenTulokset) {
+        for (HakemusWrapper hakemusWrapper : samanToiveenTulokset) {
+            if (prioriteeteissaOnRistiriita(samanToiveenTulokset, hakemusWrapper)) {
+                List<String> jonoOiditJaPrioriteetit = samanToiveenTulokset.stream()
+                    .map(hw -> hw.getValintatapajono().getValintatapajono().getOid() + " : " + hw.getHakemus().getPrioriteetti())
+                    .collect(Collectors.toList());
+                throw new IllegalStateException(String.format("Hakukohteessa %s hakemuksella %s on eri prioriteetteja! %s",
+                    hakukohdeOid, hakemusWrapper.getHakemus().getHakemusOid(), jonoOiditJaPrioriteetit));
+            }
+        }
+    }
+
+    private boolean prioriteeteissaOnRistiriita(List<HakemusWrapper> samanToiveenTulokset, HakemusWrapper hakemusWrapper) {
+        return !Objects.equals(hakemusWrapper.getHakemus().getPrioriteetti(), samanToiveenTulokset.get(0).getHakemus().getPrioriteetti());
+    }
+}
