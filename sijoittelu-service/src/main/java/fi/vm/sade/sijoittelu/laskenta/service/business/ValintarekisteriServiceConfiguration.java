62e6c95a41d72d8444dfd64ec07cd2317a400686
Jani Rahkola <jani.rahkola@reaktor.com>
2020-06-03 15:04:57 +0300

    Lisää puuttuvat VTS propertyt

--- sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/ValintarekisteriServiceConfiguration.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/ValintarekisteriServiceConfiguration.java
@@ -16,7 +16,12 @@ public class ValintarekisteriServiceConfiguration {
                                                 @Value("${valintarekisteri.tarjonta-service.url}") String tarjontaUrl,
                                                 @Value("${valintarekisteri.organisaatio-service.url") String organisaatioUrl,
                                                 @Value("${valintarekisteri.parseleniently.tarjonta}") String tarjontaParseLeniently,
-                                                @Value("${valintarekisteri.koodisto-service.url}") String koodistoUrl) {
+                                                @Value("${valintarekisteri.koodisto-service.url}") String koodistoUrl,
+                                                @Value("${valintarekisteri.cas.username}") String casUsername,
+                                                @Value("${valintarekisteri.cas.password}") String casPassword,
+                                                @Value("${valintarekisteri.blaze.response-header-timeout}") String blazeResponseHeaderTimeout,
+                                                @Value("${valintarekisteri.blaze.idle-timeout}") String blazeIdleTimeout,
+                                                @Value("${valintarekisteri.blaze.request-timeout}") String blazeRequestTimeout) {
         this.properties = new Properties();
         this.properties.setProperty("valinta-tulos-service.valintarekisteri.db.user", valintarekisteriDbUser);
         this.properties.setProperty("valinta-tulos-service.valintarekisteri.db.password", valintarekisteriDbPassword);
@@ -25,6 +30,11 @@ public class ValintarekisteriServiceConfiguration {
         this.properties.setProperty("organisaatio-service.url", organisaatioUrl);
         this.properties.setProperty("valinta-tulos-service.parseleniently.tarjonta", tarjontaParseLeniently);
         this.properties.setProperty("koodisto-service.rest.url", koodistoUrl);
+        this.properties.setProperty("valinta-tulos-service.cas.username", casUsername);
+        this.properties.setProperty("valinta-tulos-service.cas.password", casPassword);
+        this.properties.setProperty("valinta-tulos-service.blaze.response-header-timeout", blazeResponseHeaderTimeout);
+        this.properties.setProperty("valinta-tulos-service.blaze.idle-timeout", blazeIdleTimeout);
+        this.properties.setProperty("valinta-tulos-service.blaze.request-timeout", blazeRequestTimeout);
     }
 
     public Properties getProperties() {
