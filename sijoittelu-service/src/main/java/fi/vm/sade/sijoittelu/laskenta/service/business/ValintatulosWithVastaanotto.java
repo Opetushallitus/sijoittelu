f4afd8a0bec799f3076172a0d6114580d1d1677c
Santeri Korri <santeri.korri@knowit.fi>
2024-12-07 09:03:19 +0200

    OY-4985 Päivitetty Spring Boot versio

--- sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/ValintatulosWithVastaanotto.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/sijoittelu/laskenta/service/business/ValintatulosWithVastaanotto.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.WebApplicationException;
 import java.util.*;
 import java.util.stream.Collectors;
 
