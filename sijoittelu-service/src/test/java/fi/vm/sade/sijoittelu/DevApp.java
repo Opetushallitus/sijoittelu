a9cb9ab77a602ab82e4ad50f0bc4fc6ef7f6d3d8
Tomi Katajanheimo <tomi@katajanheimo.com>
2026-02-10 15:29:11 +0200

    OY-5472 kirjastopäivitykset

--- sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/DevApp.java
+++ sijoittelu-service/src/test/java/fi/vm/sade/sijoittelu/DevApp.java
@@ -1,7 +1,6 @@
 package fi.vm.sade.sijoittelu;
 
 import fi.vm.sade.util.TempDockerDB;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Profile;
