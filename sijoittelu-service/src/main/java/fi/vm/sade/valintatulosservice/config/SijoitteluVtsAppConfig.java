274022ae02818ad7682e00237a3664b177d9fbd8
Tomi Katajanheimo <tomi@katajanheimo.com>
2026-04-27 09:47:14 +0300

    Korjaa VTS config

--- sijoittelu-service/src/main/java/fi/vm/sade/valintatulosservice/config/SijoitteluVtsAppConfig.java
+++ sijoittelu-service/src/main/java/fi/vm/sade/valintatulosservice/config/SijoitteluVtsAppConfig.java
@@ -1,27 +1,12 @@
 package fi.vm.sade.valintatulosservice.config;
 
-import fi.vm.sade.javautils.nio.cas.CasClient;
-import fi.vm.sade.javautils.nio.cas.CasConfig;
-import fi.vm.sade.javautils.nio.cas.impl.CasClientImpl;
-import fi.vm.sade.javautils.nio.cas.impl.CasSessionFetcher;
+import fi.vm.sade.security.ProductionSecurityContext;
 import fi.vm.sade.security.SecurityContext;
-import fi.vm.sade.valintatulosservice.security.Role;
 import fi.vm.sade.valintatulosservice.valintaperusteet.ValintaPerusteetService;
 import fi.vm.sade.valintatulosservice.valintaperusteet.ValintaPerusteetServiceImpl;
-import org.asynchttpclient.AsyncHttpClient;
-import scala.Option;
-import scala.Some;
-import scala.jdk.javaapi.CollectionConverters;
 import scala.collection.immutable.Map;
 
-import java.time.Duration;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-
-import static java.time.temporal.ChronoUnit.MINUTES;
-import static java.time.temporal.ChronoUnit.SECONDS;
-import static org.asynchttpclient.Dsl.asyncHttpClient;
+import java.util.concurrent.TimeUnit;
 
 public class SijoitteluVtsAppConfig implements VtsAppConfig.VtsAppConfig, VtsAppConfig.ExternalProps, VtsAppConfig.CasSecurity {
   @Override
@@ -50,59 +35,10 @@ public class SijoitteluVtsAppConfig implements VtsAppConfig.VtsAppConfig, VtsApp
 
   @Override
   public SecurityContext securityContext() {
-    final CasConfig casConfig = new CasConfig.CasConfigBuilder(
-        settings().securitySettings().casUsername(),
-        settings().securitySettings().casPassword(),
-        settings().securitySettings().casUrl(),
-        settings().securitySettings().casServiceIdentifier(),
-        "CSRF",
-        settings().callerId(),
-        null
-    ).setJsessionName("JSESSIONID").build();
-    final AsyncHttpClient httpClient = asyncHttpClient();
-    final CasClient casClient = new CasClientImpl(
-        casConfig,
-        httpClient,
-        new CasSessionFetcher(
-            casConfig,
-            httpClient,
-            Duration.of(20, MINUTES).toMillis(),
-            Duration.of(2, SECONDS).toMillis()) {
-
-          public CompletableFuture<String> fetchSessionToken() {
-            return CompletableFuture.completedFuture("session-token-from-mock-context");
-          }
-
-        }
-    );
-    final Set<Role> roles = CollectionConverters
-        .asJavaCollection(settings().securitySettings().requiredRoles())
-        .stream()
-        .map(Role::new)
-        .collect(Collectors.toSet());
-
-    return new SecurityContext() {
-      @Override
-      public String casServiceIdentifier() {
-        return settings().securitySettings().casServiceIdentifier();
-      }
-
-      @Override
-      public scala.collection.immutable.Set<Role> requiredRoles() {
-        return CollectionConverters.asScala(roles).toSet();
-      }
-
-      @Override
-      public Option<CasClient> javaCasClient() {
-        return Some.apply(casClient);
-      }
-
-      @Override
-      public scala.concurrent.duration.Duration validateServiceTicketTimeout() {
-        return settings().securitySettings().casValidateServiceTicketTimeout();
-      }
+    return new ProductionSecurityContext(settings().securitySettings().casServiceIdentifier(),
+        scala.collection.immutable.Set$.MODULE$.empty(),
+        scala.concurrent.duration.Duration.apply(30, TimeUnit.SECONDS));
     };
-  }
 
   @Override
   public ValintaPerusteetService valintaPerusteetService() {
